d56ace9332c54229bf7d0d835f98cb491837d02c
Michael Angelo Manaog <michael.manaog@teamhgs.com>
2024-11-03 02:20:47 +0800

    implement pagination

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -3,17 +3,27 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\ContactRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\PaginatorInterface;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'contact_index')]
-    public function index(ContactRepository $contactRepository): Response
+    public function index(Request $request, ContactRepository $contactRepository, PaginatorInterface $paginator): Response
     {
-        $contacts = $contactRepository->findAll();
+        $query = $contactRepository->createQueryBuilder('c')
+            ->orderBy('c.name', 'ASC')
+            ->getQuery();
+
+        $contacts = $paginator->paginate(
+            $query,
+            $request->query->getInt('page', 1),
+            5
+        );
 
         return $this->render('contact/index.html.twig', [
             'contacts' => $contacts
